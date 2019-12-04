02d2e4343b6c5c0705e748c4fb0fafb8a3fb53c1
nimbus117 <mail@nimbus117.co.uk>
2019-12-04 21:17:24 +0000

    2019-04-2

--- 2019/04/main.js
+++ 2019/04/main.js
@@ -1,23 +1,31 @@
-const hasGroupOfSame = (digitArray) =>
-  digitArray.some((digit, i, digitArray) => digit === digitArray[i + 1]);
-
 const hasNoDecreasing = (digitArray) =>
-  !digitArray.some((digit, i, digitArray) => digit > digitArray[i + 1]);
+  !digitArray.some((digit, i, arr) => digit > arr[i + 1]);
+
+const hasAtLeast2Adjacent = (digitArray) =>
+  digitArray.some((digit, i, arr) => digit === arr[i + 1]);
+
+const has2Adjacent = (digitArray) =>
+  digitArray.some(
+    (digit, i, arr) =>
+      digit !== arr[i - 1] && digit === arr[i + 1] && digit !== arr[i + 2]
+  );
 
 const validPasswordCount = (input, validators) => {
   const [start, end] = input[0].split('-');
-  const validPasswords = [];
+  let count = 0;
 
-  for (let i = start; i <= end; i++) {
+  for (let i = start; i < end; i++) {
     const digitArray = i
       .toString()
       .split('')
       .map((x) => parseInt(x));
-
-    if (validators.every((valid) => valid(digitArray))) validPasswords.push(i);
+    if (validators.every((valid) => valid(digitArray))) count++;
   }
-  return validPasswords.length;
+  return count;
 };
 
 module.exports.part1 = (input) =>
-  validPasswordCount(input, [hasGroupOfSame, hasNoDecreasing]);
+  validPasswordCount(input, [hasNoDecreasing, hasAtLeast2Adjacent]);
+
+module.exports.part2 = (input) =>
+  validPasswordCount(input, [hasNoDecreasing, has2Adjacent]);
