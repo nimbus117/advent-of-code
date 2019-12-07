cabcce83c2f9b0797701ab5aaf9c6295dcb6d7dc
nimbus117 <mail@nimbus117.co.uk>
2019-12-07 17:21:22 +0000

    2019-06-2

--- 2019/06/index.js
+++ 2019/06/index.js
@@ -1,13 +1,52 @@
-const toTree = (input, rootNode = 'COM') =>
+const toTree = (input, rootNode, depth = 1) =>
   input
     .filter((i) => i[0] === rootNode)
-    .map((i) => ({ node: i[1], descendents: toTree(input, i[1]) }));
+    .map((i) => ({
+      value: i[1],
+      parent: rootNode,
+      descendents: toTree(input, i[1], depth + 1),
+      depth: depth
+    }));
 
-const orbitTree = (orbits) => toTree(orbits.map((i) => i.split(')')));
+const node = (tree, nodeValue) => {
+  let result;
+  for (const n of tree) {
+    result = n.value === nodeValue ? n : node(n.descendents, nodeValue);
+    if (result) break;
+  }
+  return result;
+};
 
-const countOrbits = (orbits, count = 0) =>
+const distanceBetween = (tree, node1, node2) => {
+  let count = 0;
+  const nodes = [node(tree, node1), node(tree, node2)].sort(
+    (x, y) => y.depth - x.depth
+  );
+
+  while (nodes[0].depth > nodes[1].depth) {
+    nodes[0] = node(tree, nodes[0].parent);
+    count++;
+  }
+
+  while (nodes[0].parent !== nodes[1].parent) {
+    nodes.forEach((n, i) => (nodes[i] = node(tree, n.parent)));
+    count += 2;
+  }
+
+  return count;
+};
+
+const orbitTree = (orbits) =>
+  toTree(
+    orbits.map((i) => i.split(')')),
+    'COM'
+  );
+
+const totalOrbits = (orbits, count = 0) =>
   orbits.reduce((acc, cur) => {
-    return acc + countOrbits(cur.descendents, count + 1);
+    return acc + totalOrbits(cur.descendents, count + 1);
   }, count);
 
-module.exports.part1 = (localOrbits) => countOrbits(orbitTree(localOrbits));
+module.exports.part1 = (localOrbits) => totalOrbits(orbitTree(localOrbits));
+module.exports.part2 = (localOrbits) =>
+  distanceBetween(orbitTree(localOrbits), 'SAN', 'YOU');
