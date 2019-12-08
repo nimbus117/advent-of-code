6824e9e7d7c7f54329c12611f56f48e794486667
nimbus117 <mail@nimbus117.co.uk>
2019-12-08 19:17:28 +0000

    condensed to single while loop

--- 2019/06/index.js
+++ 2019/06/index.js
@@ -1,34 +1,29 @@
 const toTree = (input, rootNode = 'COM', depth = 1) =>
   input
     .filter(([parent]) => parent === rootNode)
-    .map(([parent, child]) => ({
+    .map(([, child]) => ({
       value: child,
-      parent: parent,
+      parent: rootNode,
       descendents: toTree(input, child, depth + 1),
       depth: depth
     }));
 
 const findNode = (tree) => (nodeValue) => {
-  let result;
   for (const n of tree) {
-    result = n.value === nodeValue ? n : findNode(n.descendents)(nodeValue);
-    if (result) return result;
+    const node = n.value === nodeValue ? n : findNode(n.descendents)(nodeValue);
+    if (node) return node;
   }
 };
 
 const distanceBetween = (tree, node1, node2) => {
   let count = 0;
   const find = findNode(tree);
-  const nodes = [find(node1), find(node2)].sort((x, y) => y.depth - x.depth);
-
-  while (nodes[0].depth > nodes[1].depth) {
-    nodes[0] = find(nodes[0].parent);
-    count++;
-  }
+  const nodes = [find(node1), find(node2)];
 
   while (nodes[0].parent !== nodes[1].parent) {
-    nodes.forEach((n, i) => (nodes[i] = find(n.parent)));
-    count += 2;
+    nodes.sort((x, y) => y.depth - x.depth);
+    nodes[0] = find(nodes[0].parent);
+    count++;
   }
 
   return count;
