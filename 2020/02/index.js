99a81f825188ab799508f6c450d07e53f7d00c0d
nimbus117 <mail@nimbus117.co.uk>
2020-12-02 21:56:03 +0000

    2020-02

---
+++ 2020/02/index.js
@@ -0,0 +1,26 @@
+const parseLine = (input) => {
+  const [policy, password] = input.split(': ');
+  const [numbers, char] = policy.split(' ');
+  const [num1, num2] = numbers.split('-').map((m) => parseInt(m));
+  return { password, char, num1, num2 };
+};
+
+const validateSledPassword = ({ password, char, num1, num2 }) => {
+  const count = password.split('').filter((c) => c === char).length;
+  return count >= num1 && count <= num2;
+};
+
+const validateTobogganPassword = ({ password, char, num1, num2 }) => {
+  const chars = password.split('');
+  const num2Match = chars[num2 - 1] === char;
+  return chars[num1 - 1] === char ? !num2Match : num2Match;
+};
+
+const countValidPasswords = (validator) => (values) =>
+  values
+    .map(parseLine)
+    .map(validator)
+    .filter(Boolean).length;
+
+module.exports.part1 = countValidPasswords(validateSledPassword);
+module.exports.part2 = countValidPasswords(validateTobogganPassword);
