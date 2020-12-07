5399a600cf7ec3fe32bbc3ab49f0cbfd3ce405f9
nimbus117 <mail@nimbus117.co.uk>
2020-12-07 21:27:33 +0000

    2020-07-02

--- 2020/07/index.js
+++ 2020/07/index.js
@@ -1,33 +1,38 @@
-const clean = (s) =>
-  s
-    .trim()
-    .replace(/bags?\.?/, '')
-    .replace(/[\s\d]/g, '');
+const clean = (s) => s.replace(/(bags?\.?|[\s\d])/g, '');
 
 const parse = (input) =>
   input
     .split('\n')
     .slice(0, -1)
-    .map((a) => a.split('contain'))
-    .map((b) => [
-      clean(b[0]),
+    .map((x) => x.split('contain'))
+    .map(([name, children]) => [
+      clean(name),
       Object.fromEntries(
-        b[1]
+        children
           .split(',')
-          .filter((c) => !c.match(/no\sother/))
-          .map((d) => [clean(d), parseInt(d)])
+          .filter((x) => !x.match(/no\sother/))
+          .map((x) => [clean(x), parseInt(x)])
       )
     ]);
 
-module.exports.part1 = (input) => {
-  const parsedInput = parse(input);
-  const findAncestors = (node) =>
-    parsedInput
-      .filter(([, holds]) => Object.keys(holds).includes(node))
-      .flatMap(([bag]) => {
-        const parents = findAncestors(bag);
-        return [bag, ...parents];
-      });
+const findAncestors = (input, node) =>
+  input
+    .filter(([, children]) => Object.keys(children).includes(node))
+    .flatMap(([bag]) => {
+      const parents = findAncestors(input, bag);
+      return [bag, ...parents];
+    });
 
-  return new Set(findAncestors('shinygold')).size;
+const countDescendents = (input, node) => {
+  const [, children] = input.find(([bag]) => bag === node);
+  return [
+    ...Object.values(children),
+    ...Object.entries(children).map(
+      ([name, count]) => countDescendents(input, name) * count
+    )
+  ].reduce((a, b) => a + b, 0);
 };
+
+module.exports.part1 = (input) =>
+  new Set(findAncestors(parse(input), 'shinygold')).size;
+module.exports.part2 = (input) => countDescendents(parse(input), 'shinygold');
